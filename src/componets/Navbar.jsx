15696ab779a86f55d08ceb95ef15778cfd1fda05
Alex Kiochukov <Al2000x@gmail.com>
2022-09-05 23:04:51 +0300

    i added the renavigation from the login

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -1,30 +1,46 @@
 import React from "react";
 import "./Navbar.css";
-import { useDispatch , useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Swal from "sweetalert2";
 import { changeTheme } from "../features/theme/theme-toggle-slice";
-import {Link} from 'react-router-dom'
-import darkToggle from '../media/dark.png'
-import lightToggle from '../media/light.png'
-import darkLoggo from '../media/gamestick_dark_mode.png'
-import lightLoggo from '../media/gamestick_light_mode.png'
+import { Link } from "react-router-dom";
+import darkToggle from "../media/dark.png";
+import lightToggle from "../media/light.png";
+import darkLoggo from "../media/gamestick_dark_mode.png";
+import lightLoggo from "../media/gamestick_light_mode.png";
 
 const Navbar = () => {
-
-
-    const dispatch= useDispatch();
-    const selectTheme = useSelector(state => state.theme)
-    // navbar navbar-expand-lg p-3 navbar-bg navbar-dark d-flex
+  const dispatch = useDispatch();
+  const selectTheme = useSelector((state) => state.theme);
+  // navbar navbar-expand-lg p-3 navbar-bg navbar-dark d-flex
 
   return (
     <>
-      <nav className={selectTheme.dark ? "navbar navbar-expand-lg p-3 navbar-bg navbar-dark d-flex" : "navbar navbar-expand-lg p-3 navbar-bg navbar-light d-flex" }>
-          <Link to={'/'}>
-            <img  src={ selectTheme.dark ? darkLoggo : lightLoggo } height="50px" alt="website logo" />
-            <h3 className={selectTheme.dark ?"d-inline-block game-informer-dark":"d-inline-block game-informer-light"}>&nbsp;&nbsp;&nbsp; Game Informer</h3>
-          </Link>
+      <nav
+        className={
+          selectTheme.dark
+            ? "navbar navbar-expand-lg p-3 navbar-bg navbar-dark d-flex"
+            : "navbar navbar-expand-lg p-3 navbar-bg navbar-light d-flex"
+        }
+      >
+        <Link to={"/"}>
+          <img
+            src={selectTheme.dark ? darkLoggo : lightLoggo}
+            height="50px"
+            alt="website logo"
+          />
+          <h3
+            className={
+              selectTheme.dark
+                ? "d-inline-block game-informer-dark"
+                : "d-inline-block game-informer-light"
+            }
+          >
+            &nbsp;&nbsp;&nbsp; Game Informer
+          </h3>
+        </Link>
         <div className="flex-grow-1"></div>
-        <div >
+        <div>
           <button
             className="navbar-toggler"
             type="button"
@@ -49,7 +65,8 @@ const Navbar = () => {
                 </a>
               </li>
               <li className="nav-item dropdown">
-                <Link to={'/'}
+                <Link
+                  to={"/"}
                   className="nav-link dropdown-toggle"
                   id="navbarDropdownMenuLink"
                   data-toggle="dropdown"
@@ -82,19 +99,35 @@ const Navbar = () => {
           </div>
         </div>
         <div className="flex-grow-1 spacer"></div>
-        <img onClick={()=>{{selectTheme.dark ? Swal.fire(
-              `light theme`
-            ):Swal.fire ({
-              title: `dark theme`,
-              customClass: {
-                confirmButton: `swal-dark-confirm-btn`,
-                popup: `dark`
-              }
-            })} 
-                dispatch(changeTheme())
-              }} src={selectTheme.dark? darkToggle : lightToggle} height="30px" width='100px'/>
-              &nbsp;&nbsp;
-        <button className=" btn-custom">Login Now!</button>
+
+        {/* theme changer swal */}
+
+        <img
+          onClick={() => {
+            {
+              selectTheme.dark
+                ? Swal.fire(`light theme`)
+                : Swal.fire({
+                    title: `dark theme`,
+                    customClass: {
+                      confirmButton: `swal-dark-confirm-btn`,
+                      popup: `dark`,
+                    },
+                  });
+            }
+            dispatch(changeTheme());
+          }}
+          src={selectTheme.dark ? darkToggle : lightToggle}
+          height="30px"
+          width="100px"
+        />
+
+        {/* theme changer swal */}
+        
+        &nbsp;&nbsp;
+        <Link to={"/login"}>
+          <button className=" btn-custom">Login Now!</button>
+        </Link>
       </nav>
     </>
   );
