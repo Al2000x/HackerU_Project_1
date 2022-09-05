5bf91cfaf4537274cba7f7d3ef9bbb5ed55e9b0c
Alex Kiochukov <Al2000x@gmail.com>
2022-09-06 01:49:36 +0300

    made a login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
+import './Login.css'
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+  <>
+  <section>
+  <div className="LoginBG">
+    <div className="loginForm">
+     <h1>WELCOME TO<br />GAME INFORMER</h1>
+      <h2 >enter username</h2>
+      <input className="mt-3 loginInput" type="text" /><br />
+      <button className="mt-3 glow-on-hover loginButton">Begin your journey</button>
+    </div>
+    </div>
+    </section>
+  </>
+  );
+};
 
-export default Login
+export default Login;
+// <div>
+//   <section>
+//     <div>
+//       <img src="" alt="" />
+//       <h2></h2>
+//       <input type="text" /><button></button></div>
+//   </section>
+// </div>
